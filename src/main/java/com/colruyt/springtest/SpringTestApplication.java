fa48b06b10c852038b1c80535142fa844b49711c
Karan Vallakati <karan.vallakati@colruytgroup.com>
2024-07-04 23:30:03 +0530

    review

--- src/main/java/com/colruyt/springtest/SpringTestApplication.java
+++ src/main/java/com/colruyt/springtest/SpringTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringTestApplication {
-
+// todo : Overall : 6/10
     public static void main(String[] args) {
         SpringApplication.run(SpringTestApplication.class, args);
     }
--- src/test/java/com/colruyt/springtest/SpringTestApplicationTests.java
+++ src/test/java/com/colruyt/springtest/SpringTestApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringTestApplicationTests {
- // todo: the main idea of this project was to write unit tests and integration tests
+
     @Test
     void contextLoads() {
     }
